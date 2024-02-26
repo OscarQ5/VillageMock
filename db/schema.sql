299bcafdb350ea2d8e82049b69bae71ae2321a68
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2024-02-26 03:30:19 -0500

    contacts table and seed

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,14 @@ CREATE TABLE users (
     status VARCHAR(10) DEFAULT 'offline',
     profile_picture_url VARCHAR(255) DEFAULT '/static/default_profile_pic.webp'
 );
+
+CREATE TABLE contacts (
+    contact_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    status VARCHAR(10) DEFAULT 'offline',
+    profile_picture_url VARCHAR(255) DEFAULT '/static/default_profile_pic.webp',
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE
+);
--- db/seed.sql
+++ db/seed.sql
@@ -3,3 +3,10 @@
 INSERT INTO users (name, password_hash, email, phone_number) VALUES
 ('John Doe', 'h@shed_password', 'john@example.com', '123-456-7890'),
 ('Jane Smith', 'hash3d_password', 'jane@example.com', '987-654-3210');
+
+INSERT INTO contacts (name, password_hash, email, phone_number, user_id) VALUES
+('Contact 1', 'password_hash_1', 'contact1@example.com', '1234567890', 1),
+('Contact 2', 'password_hash_2', 'contact2@example.com', '9876543210', 1),
+('Contact 3', 'password_hash_3', 'contact3@example.com', '5555555555', 3),
+('Contact 4', 'password_hash_4', 'contact4@example.com', '1111111111', 2),
+('Contact 5', 'password_hash_5', 'contact5@example.com', '2222222222', 2);
