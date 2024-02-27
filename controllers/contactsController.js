231ca52fae36d34a9cb6c0e32d413539d67f689f
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2024-02-26 22:05:24 -0500

    createContact function & post route

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const contacts = express.Router({ mergeParams: true })
-const { getContacts } = require('../queries/contacts')
+const { getContacts, createContact } = require('../queries/contacts')
 
 contacts.get('/', async (req, res) => {
     const userId = req.params.user_id
     try {
         const userContacts = await getContacts(userId)
         res.status(200).json(userContacts)
+    } catch (err) {
+        res.status(404).json({ error: err })
+    }
+})
+
+contacts.post('/', async (req, res) => {
+    const newContact = req.body
+    try {
+        const createdContact = await createContact(newContact)
+        res.status(201).json(createdContact)
     } catch (err) {
         res.status(404).json({ error: err })
     }
--- queries/contacts.js
+++ queries/contacts.js
@@ -9,4 +9,17 @@ const getContacts = async (userId) => {
         return err
     }
 }
-module.exports = { getContacts }
+
+const createContact = async (newContact) => {
+    const { name, password_hash, email, phone_number, profile_picture_url, user_id } = newContact
+    const profilePic = profile_picture_url ? profile_picture_url : '/static/default_profile_pic.webp'
+    try {
+        const createdContact = await db.one("INSERT INTO contacts (name, password_hash, email, phone_number, profile_picture_url, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+            [name, password_hash, email, phone_number, profilePic, user_id])
+        return createdContact
+    } catch (err) {
+        return err
+    }
+}
+
+module.exports = { getContacts, createContact }
